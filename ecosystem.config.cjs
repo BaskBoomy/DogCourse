9c86dec2355594cba603dced7900e6cbbbd6a865
jack <whdgurtpqms@gmail.com>
2023-01-25 22:15:18 +0900

    add pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "Dog-Course-Server",
+      script: "dist/app.js",
+      instances: "max",
+      exec_mode: "cluster",
+      max_memory_restart: "256M",
+      env: {
+        NODE_ENV: "development",
+        GOOGLE_SECRET_KEY:"AIzaSyAqYgAYMn77AmEqsMimgx4fBpiq6GXpgr4",
+        NAVER_CLIENT_ID:"8vps61fg52",
+        NAVER_CLIENT_SECRET:"zkeCHVqUw89tj2QQxZo6f0bSIcVeDhSEzZCj07X4",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        GOOGLE_SECRET_KEY:"AIzaSyAqYgAYMn77AmEqsMimgx4fBpiq6GXpgr4",
+        NAVER_CLIENT_ID:"8vps61fg52",
+        NAVER_CLIENT_SECRET:"zkeCHVqUw89tj2QQxZo6f0bSIcVeDhSEzZCj07X4",
+      },
+    },
+  ],
+};
