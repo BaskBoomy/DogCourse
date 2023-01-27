06f28fdf06e621ae3568f531481589b155359718
jack <whdgurtpqms@gmail.com>
2023-01-27 18:56:07 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 echo 'cd /home/ec2-user/DogCourse' >> /home/ec2-user/DogCourse/deploy.log
 cd /home/ec2-user/DogCourse >> /home/ec2-user/DogCourse/deploy.log
 
-echo 'pm2 restart Dog-Course-Server' >> /home/ec2-user/DogCourse/deploy.log 
-# pm2 restart Dog-Course-Server  >> /home/ec2-user/DogCourse/deploy.log
+echo 'npm run start:prod' >> /home/ec2-user/DogCourse/deploy.log 
+npm run start:prod  >> /home/ec2-user/DogCourse/deploy.log
