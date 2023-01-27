fed9369ed62c9ab0b4a35ba25dfb4120508ef603
jack <whdgurtpqms@gmail.com>
2023-01-27 18:32:48 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 echo 'cd /home/ec2-user/DogCourse' >> /home/ec2-user/DogCourse/deploy.log
 cd /home/ec2-user/DogCourse >> /home/ec2-user/DogCourse/deploy.log
 
-echo 'npm run pm2' >> /home/ec2-user/DogCourse/deploy.log 
-npm run pm2  >> /home/ec2-user/DogCourse/deploy.log
+echo 'npm run start:prod' >> /home/ec2-user/DogCourse/deploy.log 
+npm run start:prod  >> /home/ec2-user/DogCourse/deploy.log
