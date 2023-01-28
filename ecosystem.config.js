0a65d418c5fd6966875433dc8a2852e358430139
jack <whdgurtpqms@gmail.com>
2023-01-28 18:29:12 +0900

    app -> main

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "Dog-Course-Server",
-      script: "dist/app.js",
+      script: "dist/main.js",
       instances: "max",
       exec_mode: "cluster",
       max_memory_restart: "256M",
