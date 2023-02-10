7e4692a10439a706b0e858c5a3d210cb940ff79e
jack <whdgurtpqms@gmail.com>
2023-02-10 17:18:22 +0900

    update script for submodule

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,8 +4,11 @@ echo 'run after_install.sh: ' >> /home/ec2-user/DogCourse/deploy.log
 echo 'cd /home/ec2-user/DogCourse' >> /home/ec2-user/DogCourse/deploy.log
 cd /home/ec2-user/DogCourse >> /home/ec2-user/DogCourse/deploy.log
 
-echo 'git submodule update --remote' >> /home/ec2-user/DogCourse/deploy.log
-git submodule update --remote
+echo 'git submodule init' >> /home/ec2-user/DogCourse/deploy.log
+git submodule init
+
+echo 'git submodule update --recursive' >> /home/ec2-user/DogCourse/deploy.log
+git submodule update --recursive
 
 echo 'npm install' >> /home/ec2-user/DogCourse/deploy.log 
 npm install >> /home/ec2-user/DogCourse/deploy.log
